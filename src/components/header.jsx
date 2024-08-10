5f0107fe19092513cc7dc25d3a7c58e27fb912ff
Sanjoy Saha <sanjoysaha0524@gmail.com>
2024-08-10 21:13:11 +0530

    profile pic upload

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,30 +10,36 @@ import { Link, useNavigate} from "react-router-dom"
 import {Button} from "./ui/button";
 import {Avatar, AvatarFallback, AvatarImage} from "@radix-ui/react-avatar";
 import { LinkIcon, LogOut } from "lucide-react";
+import { UrlState } from "@/context";
+import {logout} from "@/db/apiAuth" 
+import useFetch from "@/hooks/use-fetch";
+import { BarLoader } from "react-spinners";
 
 const Header = () => {
     const navigate = useNavigate();
-    const user = false;
+    const {user, fetchUser} = UrlState()
+    const {loading, fn: fnLogout} = useFetch(logout);
 
   return (
+    <>
     <nav className="py-4 flex justify-between items-center">
         <Link to="/">
         <img src="/logo.png" className="h-16" alt="Logo"/>
         </Link>
 
-        <div className="flex gap-4">
+        <div>
             {!user ?(
             <Button onClick={()=> navigate("/auth")}>Login</Button>
             ):(
 <DropdownMenu>
   <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
   <Avatar>
-  <AvatarImage src="https://github.com/shadcn.png" />
+  <AvatarImage src={user?.user_metadata?.profile_pic} className="object-contain"/>
   <AvatarFallback>SA</AvatarFallback>
 </Avatar>
   </DropdownMenuTrigger>
   <DropdownMenuContent>
-    <DropdownMenuLabel>Sanjoy Saha</DropdownMenuLabel>
+    <DropdownMenuLabel>{user?.user_metadata?.name}</DropdownMenuLabel>
     <DropdownMenuSeparator />
     <DropdownMenuItem>
     <Link to="/dashboard" className="flex">
@@ -43,15 +49,23 @@ const Header = () => {
     </DropdownMenuItem>
     <DropdownMenuItem className="text-red-400">
     <LogOut className="mr-2 h-4 w-4" />
-    <span>Logout</span>
+    <span 
+    onClick={() => {
+      fnLogout().then(() => {
+        fetchUser();
+        navigate("/");
+      })
+    }}
+    >
+      Logout</span>
         </DropdownMenuItem>
   </DropdownMenuContent>
 </DropdownMenu>
-
-            )
-        }
+            )}
         </div>
     </nav>
+        {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
+    </>
   )
 }
 
